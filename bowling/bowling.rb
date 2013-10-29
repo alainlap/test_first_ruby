26ff793f6973390aac81cb33b8b9871ab931062a
alainlap <alainlaplume@gmail.com>
2013-10-29 13:50:15 -0400

    passed all bowling tests

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,30 +1,37 @@
 class Game
-	
-	def score frames
-		p expand_frames frames
+	SCORE = 0
+	FRAME = 1
+
+	def score(input)
+		p frames = map_frames(input)
 		total = 0
 		frames.each_with_index do |frame, index|
+
+			if frame[FRAME] <= 9 # add up regular roles
+				total += frame[SCORE]
+			end
+
 			
-			if frame == 10
-				if index < 10
-					total += (frame + frames[index+1] + frames[index+2])
-				end
-			else
-				total += frame
+			if frame[SCORE] == 10 && frame[FRAME] <= 9 # bonus for strike
+				total += frames[index+1][SCORE] + frames[index+2][SCORE]
+			elsif ((frame[SCORE] + frames[index-1][SCORE]) == 10) && (frame[FRAME] == frames[index-1][FRAME]) # bonus for spares
+				total += frames[index+1][SCORE]
 			end
 		end
 		total
 	end
 
-	def expand_frames frames
-		rebuild = []
-		frames.each_with_index do |frame, index|
-			
-			if frame == 10 && index > 0 && index < 10
-				rebuild << 0
+	def map_frames(input)
+		frame = 0
+		frames = []
+		input.each_with_index do |score, index|
+			frames << [score, frame]
+			if (index > 0) && (frames[index][FRAME] == frames[index-1][FRAME])
+				frame += 1
+			elsif score == 10
+				 frame += 1
 			end
-			rebuild << frame
 		end
-		rebuild
+		frames
 	end
 end
