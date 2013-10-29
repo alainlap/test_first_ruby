e273280857558260cbe3588999edf98af58f9d16
alainlap <alainlaplume@gmail.com>
2013-10-29 09:55:09 -0400

    completed commmented out problems from test 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,34 @@
-def translate word
-
+def translate string
+	words = string.split(" ")
+	translated = ""
 	vowels = ["a", "i", "e", "o", "u", "y"]
+	
+	words.each_with_index do |word, index|
+		if index > 0
+			translated += " "
+		end
 
-	if vowels.include?(word[0])
-		temp = word + "ay"
-	elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
-		temp = word[2..word.length-1] + word[0..1] + "ay" 
-	elsif !vowels.include?(word[0]) && vowels.include?(word[1])
-		temp = word[1..word.length-1] + word[0] + "ay" 
+		# words starting with vowel
+		if vowels.include?(word[0])
+			translated += (word + "ay")
+		# words starting with 'qu'
+		elsif word[0..1] == "qu"
+			translated += (word[2..-1] + "qu" + "ay")
+		# words starting with consonant then 'qu'
+		elsif word[1..2] == "qu"
+			translated += (word[3..-1] + word[0..2] + "ay")
+		# words starting with 3 consonants
+		elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
+			translated += (word[3..-1] + word[0..2] + "ay")
+		# word starting with 2 consonants
+		elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
+			translated += (word[2..-1] + word[0..1] + "ay")
+		# word starting with 1 consonant
+		elsif !vowels.include?(word[0])
+			translated += (word[1..-1] + word[0] + "ay") 
+		end
 	end
+	translated
 end
+
+translate("apple")
